2d8de32f975ae243d2ae8c73f01ae37977b4d4ad
Labasse <labasse@gmail.com>
2022-02-23 15:15:16 +0100

    Add Class Library, Integrate Mockup

---
+++ ToutDouxMetier/ListeTaches.cs
@@ -0,0 +1,32 @@
+﻿namespace ToutDouxMetier
+{
+    /// <summary>
+    /// Gère une liste de tâches de type "to-do"
+    /// </summary>
+    public class ListeTaches
+    {
+        private int id = 0;
+        private Dictionary<int, Tache> listeTaches = new Dictionary<int, Tache>();
+
+        public ListeTaches(params string [] intituleTaches)
+        {
+            foreach(string intitule in intituleTaches)
+            {
+                Add(intitule);
+            }
+        }
+        public void Add(string intitule)
+        {
+            var tache = new Tache(++id, intitule);
+
+            listeTaches.Add(tache.Id, tache);
+        }
+
+        public void Remove(int id)
+        {
+            listeTaches.Remove(id);
+        }
+
+        public IEnumerable<Tache> Taches => listeTaches.Values;
+    }
+}
---
+++ ToutDouxMetier/Tache.cs
@@ -0,0 +1,17 @@
+﻿namespace ToutDouxMetier
+{
+    /// <summary>
+    /// Une tâche de liste "to-do"
+    /// </summary>
+    public class Tache
+    {
+        public Tache(int id, string intitule)
+        {
+            Id = id;
+            Intitule = intitule;
+        }
+        public int Id { get; private init; }
+        public bool Fait { get; set; }
+        public string Intitule { get; set; }
+    }
+}
